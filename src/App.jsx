2c274aee230996fe83b122119ad9c18f6b7e8586
anup1104 <20anuppatel@gmail.com>
2025-01-05 23:26:05 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,20 @@ import React from 'react'
 import Card from './components/Card.jsx'
 function App() {
   
-  
+  const data = [
+    {img:"https://images.unsplash.com/photo-1568585105565-e372998a195d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fG1hbnMlMjBwcm9mZXNzaW9uYWx8ZW58MHx8MHx8fDA%3D",name:"Anup", profession:"Coder", isFriend:false},
+    {img:"https://images.unsplash.com/photo-1529688530647-93a6e1916f5f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fG1hbnMlMjBwcm9mZXNzaW9uYWx8ZW58MHx8MHx8fDA%3D",name:"Jone", profession:"Singer",isFriend:false},
+    {img:"https://plus.unsplash.com/premium_photo-1692650759141-ed71d789ef1b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YXJ0aWVzdHxlbnwwfHwwfHx8MA%3D%3D",name:"William", profession:"Artist",isFriend:false},
+  ]
 
 
   return (
-   <Card/>
+    <div className='bg-zinc-300 flex items-center justify-center w-screen h-screen gap-5'>
+         {
+          data.map((items, idx)=><Card  {...items}/>)
+         }
+    </div>
+    // <Card/>
   )
 }
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,20 @@
 import React, { useState } from 'react'
-import { FaArrowRightLong } from "react-icons/fa6";
-function Card() {
-  const [val, setVal] = useState(false)
+
+function Card(props) {
+  const {img, name, profession, isFriend} = props
+  console.log(props) 
+  const [val, setVal] = useState(isFriend)
   return (
-    <div className='flex items-center justify-center w-screen h-screen bg-zinc-400'>
-    <div className='flex items-center justify-center bg-zinc-200 w-80 h-52 rounded-lg overflow-hidden relative'>
-    <img src="https://images.unsplash.com/photo-1700471880758-2c1011b275ca?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGZlaWxkc3xlbnwwfHwwfHx8MA%3D%3D" alt="" />
-      <img  className={` transition-all duration-500 w-full h-full rounded-lg absolute ${val === false? "translate-x-[0%]" : "translate-x-[-100%]"} `} src="https://images.unsplash.com/photo-1733690210785-8a48c7895083?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzNHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-      <img  className={`transition-all duration-500 w-full h-full rounded-lg absolute ${val === true? "translate-x-[0%]" : "translate-x-[-100%]"} `} src="https://images.unsplash.com/photo-1620917286728-06cf79161524?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8ZmVpbGRzfGVufDB8fDB8fHww" alt="" />
-      <span onClick={()=>setVal(()=>!val)} className='absolute w-6 h-6 p-1 rounded-[60%] top-[80%] bg-zinc-400'>
-          <FaArrowRightLong />
-      </span>
-    </div>
-   
+    <div className=' w-52 h-64 rounded-lg'>
+      <div className='bg-white w-full h-[63%] rounded-lg'>
+        <img className='w-full h-full object-cover rounded-t-lg' src={img} alt="" />
+      </div>
+      <div className='w-full px-3 '>
+          <h1 className='text-lg font-extrabold'>{name}</h1>
+          <p className='text-sm font-semibold'>{profession}</p>
+          <button onClick={()=>setVal(()=>!val)} className={ `${val===true ? "bg-red-500" : "bg-sky-500"} py-1 px-3 mt-2 text-zinc-50  rounded-lg`}>{val ? "Remove Friend" : "Add Friend"}</button>
+      </div>
+      
     </div>
   )
 }
